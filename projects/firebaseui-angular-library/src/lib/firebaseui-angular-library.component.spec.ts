f8bdb9946405ce39bff45d1fc3e845e6f360fa1b
Raphael Jenni <jenni.raphael@gmail.com>
2019-06-07 10:15:11 +0200

    Add some basic tests

--- projects/firebaseui-angular-library/src/lib/firebaseui-angular-library.component.spec.ts
+++ projects/firebaseui-angular-library/src/lib/firebaseui-angular-library.component.spec.ts
@@ -1,16 +1,38 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { FirebaseuiAngularLibraryComponent } from './firebaseui-angular-library.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {FirebaseuiAngularLibraryComponent} from './firebaseui-angular-library.component';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {Subject} from 'rxjs';
+import {User} from 'firebase';
+import {FirebaseuiAngularLibraryService} from './firebaseui-angular-library.service';
 
 describe('FirebaseuiAngularLibraryComponent', () => {
   let component: FirebaseuiAngularLibraryComponent;
   let fixture: ComponentFixture<FirebaseuiAngularLibraryComponent>;
+  let firebaseAuthState: Subject<User>;
 
   beforeEach(async(() => {
+    firebaseAuthState = new Subject();
+
     TestBed.configureTestingModule({
-      declarations: [ FirebaseuiAngularLibraryComponent ]
+      declarations: [FirebaseuiAngularLibraryComponent],
+      providers: [
+        {
+          provide: AngularFireAuth,
+          useValue: new class {
+            authState = firebaseAuthState.asObservable();
+          }()
+        },
+        {
+          provide: FirebaseuiAngularLibraryService,
+          useValue: new class {
+            firebaseUiInstance = {};
+          }
+        },
+        {provide: 'firebaseUIAuthConfig', useValue: {main: 'MAIN', overwritten: 'MAIN'}},
+        {provide: 'firebaseUIAuthConfigFeature', useValue: {feature: 'FEATURE', overwritten: 'FEATURE'}}
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +43,11 @@ describe('FirebaseuiAngularLibraryComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('firebaseUiConfig', () => {
+    it('should combine firebaseUiConfig with firebaseUiConfig_Feature', () => {
+      expect(component.firebaseUiConfig).toEqual({main: 'MAIN', feature: 'FEATURE', overwritten: 'FEATURE'} as any);
+    });
   });
 });
