a180a42763e30bc36d08ad2ec621563064475edf
Lorenzo Montanari <13736036+l0ll098@users.noreply.github.com>
2020-03-30 21:08:38 +0200

    Fixed a wrong import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
-import {CustomFirebaseUIAuthConfig, FirebaseUIModule, firebase, firebaseui} from 'firebaseui-angular';
+import {ExtendedFirebaseUIAuthConfig, FirebaseUIModule, firebase, firebaseui} from 'firebaseui-angular';
 import {environment} from '../environments/environment';
 import {AppRoutingModule} from './app-routing.module';
 import {AngularFireModule} from '@angular/fire';
 import {AngularFireAuthModule} from '@angular/fire/auth';
 
-const firebaseUiAuthConfig: CustomFirebaseUIAuthConfig = {
+const firebaseUiAuthConfig: ExtendedFirebaseUIAuthConfig = {
   signInFlow: 'popup',
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
