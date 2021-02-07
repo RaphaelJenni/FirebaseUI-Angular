0c7eee2e5c75cae596c2e03327324e336c46c09c
= <andreu.rosell4@gmail.com>
2021-02-07 16:28:04 +0100

    Use auth emulator

--- projects/firebaseui-angular-library/src/lib/firebaseui-angular-library.service.ts
+++ projects/firebaseui-angular-library/src/lib/firebaseui-angular-library.service.ts
@@ -1,6 +1,10 @@
-import {Inject, Injectable, NgZone, Optional} from '@angular/core';
+import { Inject, Injectable, InjectionToken, NgZone, Optional } from '@angular/core';
 import * as firebaseui from 'firebaseui';
 import {FIREBASE_APP_NAME, FIREBASE_OPTIONS, FirebaseApp, FirebaseAppConfig, FirebaseOptions, ɵfirebaseAppFactory} from '@angular/fire';
+import _firebase from 'firebase/app';
+import { USE_EMULATOR as USE_AUTH_EMULATOR } from '@angular/fire/auth';
+
+type UseEmulatorArguments = [string, number];
 
 @Injectable()
 export class FirebaseuiAngularLibraryService {
@@ -8,12 +12,20 @@ export class FirebaseuiAngularLibraryService {
 
   constructor(@Inject(FIREBASE_OPTIONS) options: FirebaseOptions,
               @Optional() @Inject(FIREBASE_APP_NAME) nameOrConfig: string | FirebaseAppConfig | null | undefined,
+              @Optional() @Inject(USE_AUTH_EMULATOR) _useEmulator: any, // can't use the tuple here
               zone: NgZone) {
     // noinspection JSNonASCIINames
     const app: FirebaseApp = ɵfirebaseAppFactory(options, zone, nameOrConfig);
     // store the firebaseui instance on the window object to prevent double initialization
+
+    const useEmulator: UseEmulatorArguments | null = _useEmulator;
+
     if (!(<any>window).firebaseUiInstance) {
-      (<any>window).firebaseUiInstance = new firebaseui.auth.AuthUI(app.auth());
+      const auth: _firebase.auth.Auth = app.auth();
+      if (useEmulator) {
+        auth.useEmulator(`http://${useEmulator.join(':')}`);
+      }
+      (<any>window).firebaseUiInstance = new firebaseui.auth.AuthUI(auth);
     }
     this.firebaseUiInstance = (<any>window).firebaseUiInstance as firebaseui.auth.AuthUI;
   }
